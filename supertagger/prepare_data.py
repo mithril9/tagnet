21a9992a63ec075fbc7381f49e18cf974f4171e4
John <john@wluper.com>
2020-05-23 14:57:29 +0100

    update

--- supertagger/prepare_data.py
+++ supertagger/prepare_data.py
@@ -1,14 +1,29 @@
 """Preprocesses data for the supertagger"""
 
 import torch
+import pdb
+from typing import List,  Tuple
 
-def create_ix_mappings(training_data: tuple[list, list]):
-    """training_data is a tuple of 2 lists and looks as follows
+def data_to_tuples(word_file_name, tag_file_name):
+
+    sentences = open(word_file_name).readlines()
+    tag_sequences = open(tag_file_name).readlines()
+    while '' in sentences:
+        sentences.remove('')
+    while '' in tag_sequences:
+        tag_sequences.remove('')
+    data = [([word for word in sentences[i].strip().split()], [tag for tag in tag_sequences[i].strip().split()]) for i in range(len(sentences))]
+    for sentence, tag_sequence in data:
+        if len(sentence) != len(tag_sequence):
+            raise Exception("sentence and tag sequence lengths do not line up!!")
+    return data
+
+def create_ix_mappings(data: List[Tuple[List, List]]):
+
+    """data is a tuple of 2 lists and looks as follows
     (POS tags are  shown but these could be any tags including CCG supertags):
-    [
-    (["The", "dog", "ate", "the", "apple"], ["DET", "NN", "V", "DET", "NN"]),
-    (["Everybody", "read", "that", "book"], ["NN", "V", "DET", "NN"])
-    ]"""
+    [(["The", "dog", "ate", "the", "apple"], ["DET", "NN", "V", "DET", "NN"]),
+    (["Everybody", "read", "that", "book"], ["NN", "V", "DET", "NN"])]"""
 
     word_to_ix = {}
     char_to_ix = {}
--- supertagger/train.py
+++ supertagger/train.py
@@ -1,50 +1,54 @@
 """Code for training the model"""
 
-from .lstm_model import LSTMTagger
+from lstm_model import LSTMTagger
+from config import *
+from prepare_data import *
 import torch.nn as nn
 import torch.optim as optim
+import argparse
+import os
+import pdb
 
-model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix), CHAR_EMBEDDING_DIM, CHAR_HIDDEN_DIM, len(char_to_ix))
-loss_function = nn.NLLLoss()
-optimizer = optim.SGD(model.parameters(), lr=0.1)
 
-# See what the scores are before training
-# Note that element i,j of the output is the score for tag j for word i.
-# Here we don't need to train, so the code is wrapped in torch.no_grad()
+def main(data_path):
+
+    train_words_path = os.path.join(data_path, "train.words")
+    train_tags_path = os.path.join(data_path, "train.tags")
+    data = data_to_tuples(train_words_path, train_tags_path)
+    pdb.set_trace()
+    model = LSTMTagger(EMBEDDING_DIM, HIDDEN_DIM, len(word_to_ix), len(tag_to_ix), CHAR_EMBEDDING_DIM, CHAR_HIDDEN_DIM, len(char_to_ix))
+    loss_function = nn.NLLLoss()
+    optimizer = optim.SGD(model.parameters(), lr=0.1)
+
+    for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
+        for sentence, tags in training_data:
+            # Step 1. Remember that Pytorch accumulates gradients.
+            # We need to clear them out before each instance
+            model.zero_grad()
+
+            # Also, we need to clear out the hidden state of the LSTM,
+            # detaching it from its history on the last instance.
+            model.hidden = model.init_hidden()
+
+            # Step 2. Get our inputs ready for the network, that is, turn them into
+            # Tensors of word indices.
+            sentence_in = prepare_sequence(sentence, word_to_ix)
+            words_in = [prepare_sequence(word, char_to_ix) for word in sentence]
+            targets = prepare_sequence(tags, tag_to_ix)
+
+            # Step 3. Run our forward pass.
+            tag_scores = model(sentence_in, words_in, CHAR_EMBEDDING_DIM, CHAR_HIDDEN_DIM)
+
+            # Step 4. Compute the loss, gradients, and update the parameters by
+            #  calling optimizer.step()
+            loss = loss_function(tag_scores, targets)
+            loss.backward(retain_graph=True)
+            optimizer.step()
+
+# See what the scores are after training
 #with torch.no_grad():
     #inputs = prepare_sequence(training_data[0][0], word_to_ix)
     #tag_scores = model(inputs)
-    #print(tag_scores)
-
-for epoch in range(300):  # again, normally you would NOT do 300 epochs, it is toy data
-    for sentence, tags in training_data:
-        # Step 1. Remember that Pytorch accumulates gradients.
-        # We need to clear them out before each instance
-        model.zero_grad()
-
-        # Also, we need to clear out the hidden state of the LSTM,
-        # detaching it from its history on the last instance.
-        model.hidden = model.init_hidden()
-
-        # Step 2. Get our inputs ready for the network, that is, turn them into
-        # Tensors of word indices.
-        sentence_in = prepare_sequence(sentence, word_to_ix)
-        words_in = [prepare_sequence(word, char_to_ix) for word in sentence]
-        targets = prepare_sequence(tags, tag_to_ix)
-
-        # Step 3. Run our forward pass.
-        tag_scores = model(sentence_in, words_in, CHAR_EMBEDDING_DIM, CHAR_HIDDEN_DIM)
-
-        # Step 4. Compute the loss, gradients, and update the parameters by
-        #  calling optimizer.step()
-        loss = loss_function(tag_scores, targets)
-        loss.backward(retain_graph=True)
-        optimizer.step()
-
-# See what the scores are after training
-with torch.no_grad():
-    inputs = prepare_sequence(training_data[0][0], word_to_ix)
-    tag_scores = model(inputs)
 
     # The sentence is "the dog ate the apple".  i,j corresponds to score for tag j
     # for word i. The predicted tag is the maximum scoring tag.
@@ -52,4 +56,13 @@ with torch.no_grad():
     # since 0 is index of the maximum value of row 1,
     # 1 is the index of maximum value of row 2, etc.
     # Which is DET NOUN VERB DET NOUN, the correct sequence!
-    print(tag_scores)
+    #print(tag_scores)
+
+
+if __name__ == '__main__':
+    cmd_parser = argparse.ArgumentParser(description='command line arguments.')
+    cmd_parser.add_argument('--data-path', dest='data_path', type=str, nargs=1, help='the path to the folder containing the data files')
+
+    args = cmd_parser.parse_args()
+
+    main(args.data_path[0])

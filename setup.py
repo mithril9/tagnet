7c6f57905d90bd55ded7ba0c6d44011085e31cb9
John <john@wluper.com>
2020-06-20 13:28:15 +0100

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tagnet", # Replace with your own username
-    version="1.0",
+    version="1.0.0",
     author="John Torr",
     author_email="john@cantab.net",
     description="A neural network supertagger",
